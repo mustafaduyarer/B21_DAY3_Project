639b4f20eb03225dd2b028355a92f79cf99cff8f
mustafaduyarer <aydinlikkonsept@gmail.com>
2021-02-04 17:41:17 +0100

    working on nice message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git last day");
         System.out.println("second commit");
+        //eu4 is excited=====
     }
 }
