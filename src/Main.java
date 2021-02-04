66ce3fd53a1b545a644879e0f52a40165a1c1c1d
mustafaduyarer <aydinlikkonsept@gmail.com>
2021-02-04 17:43:29 +0100

    working on nice commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello git last day");
         System.out.println("second commit");
         //eu4 is excited=====
+        //Eu4 group21 is very nice group
     }
 }
